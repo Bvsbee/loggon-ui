97687705e27fa95fda76603612df6424ed6aadf7
NiNisBEAN <sandestrell@gmail.com>
2025-02-25 13:24:14 -0500

    layout

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd"
+
+const Layout = () => {
+return(
+    <div>
+      <Button>layout</Button>
+    </div>
+)
+
+}
+
+export default Layout
--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "./screens/Login.tsx";
 import Test from "./screens/Test.tsx";
 import Signup from "./screens/Signup.tsx";
+import Layout from "./components/layout/layout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}></Route>
+        <Route element={<Layout />}><Route path="/" element={<App />}></Route></Route>
+        
         <Route path="/Login" element={<Login />}></Route>
         <Route path="/test" element={<Test />}></Route>
         <Route path="/Signup" element={<Signup />}></Route>
