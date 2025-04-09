254b14de2bb771e61a7bf37d65e8037487898a85
E-Roon <ecfrancis45@gmail.com>
2025-04-09 16:26:11 -0400

    Current file for fetching images

---
+++ src/components/Admin/S3ImageKey.tsx
@@ -0,0 +1,24 @@
+import {useEffect, useState } from 'react';
+
+//For new products added by admin
+const S3Image = ({ imageKey }: { imageKey: string }) => {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchImageUrl = async () => {
+      const response = await fetch(`/api/files/${imageKey}`); // Adjust if your API is proxied
+      const data = await response.json();
+      setImageUrl(data.url);
+    };
+
+    fetchImageUrl();
+  }, [imageKey]);
+
+  return imageUrl ? (
+    <img src={imageUrl} alt="S3 File" style={{ maxWidth: '100%' }} /> //Will be displayed on products page 
+  ) : (
+    <p>Loading image...</p>
+  );
+};
+
+export default S3Image;
