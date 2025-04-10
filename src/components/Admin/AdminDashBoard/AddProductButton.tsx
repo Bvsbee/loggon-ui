5123c500962c46d9e0670b377d55abd00366ef74
E-Roon <ecfrancis45@gmail.com>
2025-04-10 13:27:03 -0400

    Added Post functionality to product button file,

--- src/components/Admin/AdminDashBoard/AddProductButton.tsx
+++ src/components/Admin/AdminDashBoard/AddProductButton.tsx
@@ -38,9 +38,41 @@ const AddProductButton = ({
 
   const [form] = Form.useForm();
 
+  //AWS Post command 
+  const [file, setFile] = useState<File | null>(null);
+  const [uploadStatus, setUploadStatus] = useState('');
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await res.json();
+      setUploadStatus(`✅ Uploaded: ${data.key}`);
+    } catch (err) {
+      setUploadStatus('❌ Upload failed');
+    }
+    
+  };
+    
+
   const onSubmit = () => {
     form.submit();
     handleModalVisibility();
+    handleUpload();
   };
 
   const onFinish = async (values: Product) => {
@@ -76,8 +108,12 @@ const AddProductButton = ({
   return (
     <Content>
       
-      <Button onClick={handleModalVisibility}>Add New Product</Button>
+      <Button onClick={handleModalVisibility} >Add New Product</Button>
       <Modal open={visible} onOk={onSubmit} onCancel={handleModalVisibility}>
+        <>
+          <input type="file" accept="image/*" onChange={handleFileChange} />
+          <button onClick={handleUpload}>Upload Image</button>
+          {uploadStatus && <p>{uploadStatus}</p>}
         <Form form={form} onFinish={onFinish}>
           <Row gutter={6}>
             <Col span={20}>
@@ -172,6 +208,7 @@ const AddProductButton = ({
             </Col>
           </Row>
         </Form>
+        </>
       </Modal>
     </Content>
   );
