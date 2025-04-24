837d78bdc370125a462b75f1a5dbe068728d2e6d
Michael Busbee <busbee.michael101@gmail.com>
2025-04-24 13:07:21 -0400

    WIP Adding AdminDashboard stats and checkout Process

--- src/admin/AdminDashboard.tsx
+++ src/admin/AdminDashboard.tsx
@@ -1,5 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "../context/AuthContext";
+import fetchAdminDashboard from "../api/admin/fetchAdminDashBoard";
+
 const AdminDashboard = () => {
-  return <div></div>;
+  const { user } = useAuth();
+
+  const { data, isLoading, error, isError } = useQuery({
+    queryKey: ["adminDashboard"],
+    queryFn: () => fetchAdminDashboard(user?.id),
+  });
+
+  console.log(data);
+
+  return (
+    <div style={{ padding: "1rem" }}>
+      <h2>Admin Dashboard</h2>
+      <div style={{ marginTop: "1rem" }}>
+        <p>
+          <strong>Total Users:</strong> {data?.totalUsers}
+        </p>
+        <p>
+          <strong>Total Products:</strong> {data?.totalProducts}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
